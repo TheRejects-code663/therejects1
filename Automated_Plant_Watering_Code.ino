f312a7715baa86db1fba2e1715c8a078db227e32
aadivshah123456789 <157196592+aadivshah123456789@users.noreply.github.com>
2025-01-12 09:58:33 +0530

    FInal Code

--- Automated_Plant_Watering_Code.ino
+++ Automated_Plant_Watering_Code.ino
@@ -1,95 +1,74 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-
-
-const int soilMoisturePin = A0;    
-const int pumpRelayPin = 5;        
-const int moistureThreshold = 20; 
 #define DEBUG true
-const unsigned long pumpTimeout = 10; 
-unsigned long previousMillis = 0;      
-bool pumpActive = false;                
-LiquidCrystal_I2C lcd(0x27, 16, 2);     
+
+const int soilMoisturePin = A0;   
+const int pumpRelayPin = 5;        
+const int moistureThreshold = 20;  
+const unsigned long pumpTimeout = 1000;                            
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);      
 
 void setup() {
-    pinMode(pumpRelayPin, OUTPUT);
-    digitalWrite(pumpRelayPin, LOW);  
+  pinMode(pumpRelayPin, OUTPUT);
+  digitalWrite(pumpRelayPin, LOW); 
+  Serial.begin(9600);
 
-    Serial.begin(9600); 
-    lcd.init();         
-    lcd.backlight();    
+  lcd.init();
+  lcd.backlight();
+  displayMessage("System loading...");
 
-    displayMessage("System loading...");
-    delay(3000);        
-    lcd.clear();      
+  delay(3000);
+  lcd.clear();
 }
 
 void loop() {
-    int soilMoistureValue = readSoilMoisture(); 
-    updateLCD(soilMoistureValue);              
-    controlPump(soilMoistureValue);           
-
-   
-    unsigned long currentMillis = millis();
-    if (pumpActive && (currentMillis - previousMillis >= pumpTimeout)) {
-        digitalWrite(pumpRelayPin, LOW); 
-        pumpActive = false;             
-        lcd.setCursor(0, 1);
-        lcd.print("Pump OFF - Timeout");
-        Serial.println("Pump OFF due to timeout.");
-    }
+  int soilMoistureValue = readSoilMoisture();
+  updateLCD(soilMoistureValue);
+  controlPump(soilMoistureValue); 
+  delay(2000); 
 }
 
 int readSoilMoisture() {
-    int soilMoistureValue = 0;
+  int soilMoistureValue = 0;
 
-    for (int i = 0; i < 10; i++) {
-        soilMoistureValue += analogRead(soilMoisturePin);
-        delay(10); 
-    }
-    soilMoistureValue /= 10; 
-
-    if (DEBUG) {
-        Serial.print("Soil Moisture Value: ");
-        Serial.println(soilMoistureValue);
-    }
-
-    return soilMoistureValue;
+  for (int i = 0; i < 10; i++) {
+    soilMoistureValue += analogRead(soilMoisturePin);
+    delay(10); 
+  }
+  soilMoistureValue /= 10; 
+  if (DEBUG) {
+    Serial.print("Soil Moisture: ");
+    Serial.println(soilMoistureValue);
+  }
+  
+  return soilMoistureValue;
 }
 
 void updateLCD(int soilMoistureValue) {
-    lcd.clear(); 
+  lcd.clear();
+  lcd.setCursor(0, 0);
 
-    if (soilMoistureValue >= moistureThreshold) {
-        displayMessage("Soil Dry!");
-    } else {
-        displayMessage("Soil Good");
-    }
-
-    if (DEBUG) {
-        Serial.print("LCD Update - Soil Moisture: ");
-        Serial.println(soilMoistureValue);
-    }
+  if (soilMoistureValue >= moistureThreshold) {
+    lcd.print("Soil Dry!");
+  } else {
+    lcd.print("Soil Good");
+  }
 }
 
 void displayMessage(const char* message) {
-    lcd.setCursor(0, 0); 
-    lcd.print(message);  
+  lcd.setCursor(0, 0);
+  lcd.print(message);
 }
 
 void controlPump(int soilMoistureValue) {
-    if (soilMoistureValue >= moistureThreshold && !pumpActive) {
-        digitalWrite(pumpRelayPin, HIGH); 
-        pumpActive = true;              
-        previousMillis = millis();      
-        lcd.setCursor(0, 1);
-        lcd.print("Pump ON - Dry soil");
-        Serial.println("Pump ON - Soil is dry.");
-    } else if (soilMoistureValue < moistureThreshold && pumpActive) {
-        digitalWrite(pumpRelayPin, LOW); 
-        pumpActive = false;              
-        lcd.setCursor(0, 1);
-        lcd.print("Pump OFF - Wet soil");
-        Serial.println("Pump OFF - Soil is wet.");
-    }
+  if (soilMoistureValue >= moistureThreshold) {
+    digitalWrite(pumpRelayPin, HIGH); 
+    lcd.setCursor(0, 1);
+    lcd.print("Pump ON - Dry Soil");
+  } else {
+    digitalWrite(pumpRelayPin, LOW); 
+    lcd.setCursor(0, 1);
+    lcd.print("Pump OFF - Wet Soil");
+  }
 }
